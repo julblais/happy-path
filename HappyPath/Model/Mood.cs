31f085010f716d0d237df7d271805b64816943c5
Julien <sharpshark13@gmail.com>
2025-08-19 23:24:15 -0400

    Added categories.

--- HappyPath/Model/Mood.cs
+++ HappyPath/Model/Mood.cs
@@ -1,8 +1,16 @@
-﻿namespace HappyPath.Model;
+﻿using System.Text.Json.Serialization;
 
+namespace HappyPath.Model;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum Mood
 {
+    [JsonStringEnumMemberName("happy")]
     Neutral = 0,
+    
+    [JsonStringEnumMemberName("neutral")]
     Happy,
+    
+    [JsonStringEnumMemberName("sad")]
     Sad
 }
