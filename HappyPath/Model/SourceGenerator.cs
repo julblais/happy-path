72c0fc84f5c363e8a91800a1755e29e33811c49f
Julien <sharpshark13@gmail.com>
2025-08-20 21:12:28 -0400

    Added source generator.

---
+++ HappyPath/Model/SourceGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HappyPath.Model;
+
+[JsonSourceGenerationOptions(WriteIndented = true)]
+[JsonSerializable(typeof(Mood))]
+[JsonSerializable(typeof(Result))]
+internal partial class SourceGenerationContext : JsonSerializerContext
+{
+}
