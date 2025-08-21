f7929c713d99eb83ddfcc91b9e0e37fcb70e8bf4
Julien <sharpshark13@gmail.com>
2025-08-20 22:35:55 -0400

    Declare static dependency.

---
+++ HappyPath/Layout/MainLayout.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Diagnostics.CodeAnalysis;
+
+namespace HappyPath.Layout;
+
+public partial class MainLayout : LayoutComponentBase
+{
+    [DynamicDependency(DynamicallyAccessedMemberTypes.PublicConstructors, typeof(MainLayout))]
+    public MainLayout() { }
+}
